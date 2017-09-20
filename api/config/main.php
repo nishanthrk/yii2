9a227d9c6ad3ff03ce0d2d3c584d0ffb9b722948
Nishanth <jokemeblack@gmail.com>
2017-09-20 14:15:24 +0530

    master
    Pushing Master

---
+++ api/config/main.php
@@ -0,0 +1,36 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../api/config/params.php'),
+    require(__DIR__ . '/../../api/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'api\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+    ],
+    'params' => $params,
+];
---
+++ api/controllers/SiteController.php
@@ -0,0 +1,13 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        print_r("Hi");
+    }
+}
